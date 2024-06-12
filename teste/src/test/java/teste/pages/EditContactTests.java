77af9309e7fecc1392abd514589523a2ccff5120
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-06-12 18:31:58 -0300

    test: create shouldEditTheNameOfAContact file.

--- teste/src/test/java/teste/pages/EditContactTests.java
+++ teste/src/test/java/teste/pages/EditContactTests.java
@@ -14,5 +14,33 @@ class EditContactTests extends BaseTest {
     private ConsultContactPage consultContactPage;
     private AddContactPage addContactPage;
 
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+        landingPage = new LandingPage(driver);
+        consultContactPage = new ConsultContactPage(driver);
+        addContactPage = new AddContactPage(driver);
+    }
+
+    @Test
+    @DisplayName("Should edit the name of a contact")
+    void shouldEditTheNameOfAContact() {
+        landingPage.open();
+        landingPage.clickAddButton();
+        addContactPage.enterName(faker.leagueOfLegends().champion());
+        addContactPage.enterPhone(faker.phoneNumber().cellPhone());
+        addContactPage.clickCreate();
+        acceptAlert("Contato adicionado com sucesso!");
+        landingPage.open();
+        landingPage.clickConsultButton();
+        consultContactPage.clickFirstReadButton();
+        WebElement nameInput = consultContactPage.getFirstContactNameInput();
+        nameInput.click();
+        nameInput.clear();
+        nameInput.sendKeys(faker.name().firstName());
+        consultContactPage.clickFirstReadButton();
+        acceptAlert("Contato atualizado com sucesso!");
+    }
+
 }
 
