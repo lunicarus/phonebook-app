6821bc74f4fdfe573bea579079bff47014d9bc5d
João Marcelo Simão de Castro <joao.castro@usp.br>
2024-06-12 19:25:52 -0300

    test: create DeleteContactTest file

---
+++ teste/src/test/java/teste/pages/DeleteContactTest.java
@@ -0,0 +1,14 @@
+
+package teste.pages;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class DeleteContactTest extends BaseTest {
+    }
+}
