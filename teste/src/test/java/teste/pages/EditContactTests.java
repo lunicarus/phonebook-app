416ed9741ca78408c494b7df3315dd9cdb5dd54e
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-06-12 18:33:19 -0300

    test: create shouldEditTheNameAndPhoneOfAContact.

--- teste/src/test/java/teste/pages/EditContactTests.java
+++ teste/src/test/java/teste/pages/EditContactTests.java
@@ -62,5 +62,29 @@ class EditContactTests extends BaseTest {
         acceptAlert("Contato atualizado com sucesso!");
     }
 
+    @Test
+    @DisplayName("Should edit name and phone number of a contact")
+    void shouldEditNameAndPhoneOfAContact() {
+        landingPage.open();
+        landingPage.clickAddButton();
+        addContactPage.enterName(faker.leagueOfLegends().champion());
+        addContactPage.enterPhone(faker.phoneNumber().cellPhone());
+        addContactPage.clickCreate();
+        acceptAlert("Contato adicionado com sucesso!");
+        landingPage.open();
+        landingPage.clickConsultButton();
+        consultContactPage.clickFirstReadButton();
+        WebElement phoneNumberInput = consultContactPage.getFirstContactPhoneInput();
+        phoneNumberInput.click();
+        phoneNumberInput.clear();
+        phoneNumberInput.sendKeys(faker.phoneNumber().cellPhone());
+        WebElement nameInput = consultContactPage.getFirstContactNameInput();
+        nameInput.click();
+        nameInput.clear();
+        nameInput.sendKeys(faker.name().firstName());
+        consultContactPage.clickFirstReadButton();
+        acceptAlert("Contato atualizado com sucesso!");
+    }
+
 }
 
